1e6b3d052521658feccb28912752bf54bafa2bd5
Antoine Stewart <Stewartant12@gmail.com>
2025-10-17 08:29:03 -0500

    Added alternating X and O placement

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,3 +4,21 @@ window.addEventListener('DOMContentLoaded', () => {
     square.classList.add('square');
   });
 });
+
+let currentPlayer = 'X';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const squares = document.querySelectorAll('#board div');
+  
+  squares.forEach(square => {
+    square.classList.add('square');
+    
+    square.addEventListener('click', () => {
+      if (square.textContent === '') { // prevent overwriting
+        square.textContent = currentPlayer;
+        square.classList.add(currentPlayer);
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+      }
+    });
+  });
+});
