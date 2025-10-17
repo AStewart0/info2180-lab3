fee4b81de3be28a300e2b00a3b8a457aaeda500f
Antoine Stewart <Stewartant12@gmail.com>
2025-10-17 08:46:52 -0500

    Added win detection and winner message

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -17,6 +17,8 @@ window.addEventListener('DOMContentLoaded', () => {
       if (square.textContent === '') { // prevent overwriting
         square.textContent = currentPlayer;
         square.classList.add(currentPlayer);
+
+        if (checkWinner()) return;
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
       }
     });
@@ -31,3 +33,26 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+function checkWinner() {
+  const squares = Array.from(document.querySelectorAll('#board div'));
+  const combos = [
+    [0,1,2], [3,4,5], [6,7,8], // rows
+    [0,3,6], [1,4,7], [2,5,8], // cols
+    [0,4,8], [2,4,6]           // diagonals
+  ];
+
+  for (let combo of combos) {
+    const [a,b,c] = combo;
+    if (squares[a].textContent &&
+        squares[a].textContent === squares[b].textContent &&
+        squares[a].textContent === squares[c].textContent) {
+      const winner = squares[a].textContent;
+      const status = document.getElementById('status');
+      status.textContent = `Congratulations! ${winner} is the Winner!`;
+      status.classList.add('you-won');
+      return true;
+    }
+  }
+  return false;
+}
